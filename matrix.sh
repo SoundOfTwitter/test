213f87e399f84b1bbff741d1b09238bca743e37e
SoundOfTwitter <140434271+SoundOfTwitter@users.noreply.github.com>
2024-11-06 09:31:06 +0800

    Update matrix.sh

--- matrix.sh
+++ matrix.sh
@@ -2,8 +2,6 @@
 
 # 变量设置
 server_IP=$(wget -qO- ifconfig.me)
-# read -p "请输入 server_IP: " server_IP
-# read -p "请输入 strong_passwd: " strong_passwd
 strong_passwd=$(< /dev/urandom tr -dc 'A-Za-z0-9' | head -c 26)
 
 apt update && apt install -y curl gnupg2 software-properties-common lsb-release ca-certificates && curl -L https://packages.matrix.org/debian/matrix-org-archive-keyring.gpg | apt-key add - && echo "deb https://packages.matrix.org/debian/ $(lsb_release -cs) main" | tee /etc/apt/sources.list.d/matrix-org.list && apt update
@@ -62,16 +60,73 @@ systemctl restart nginx
 
 sleep 20
 
-sed -i "14c server_name: \"$server_IP\"" /etc/matrix-synapse/homeserver.yaml
+# 配置 /etc/matrix-synapse/homeserver.yaml
+# 检查是否传入了变量 server_IP 和 strong_passwo
+#if [ -z "$server_IP" ] || [ -z "$strong_passwo" ]; then
+    #echo "请设置 server_IP 和 strong_passwo 变量"
+    #exit 1
+#fi
 
-echo 'trusted_proxies:' | tee -a /etc/matrix-synapse/homeserver.yaml
+# 定义 homeserver.yaml 文件路径
+HOMESERVER_FILE="/etc/matrix-synapse/homeserver.yaml"
 
-echo '  - 127.0.0.1' | tee -a /etc/matrix-synapse/homeserver.yaml
+# 备份原始文件
+#cp "$HOMESERVER_FILE" "${HOMESERVER_FILE}.bak"
 
-# sed -i '25i\      - names: [admin]\n        compress: false' /etc/matrix-synapse/homeserver.yaml
+# 写入新的内容
+cat > "$HOMESERVER_FILE" <<EOL
+# Configuration file for Synapse. 
+#
+# This is a YAML file: see [1] for a quick introduction. Note in particular
+# that *indentation is important*: all the elements of a list or dictionary
+# should have the same indentation.
+#
+# [1] https://docs.ansible.com/ansible/latest/reference_appendices/YAMLSyntax.html
+#
+# For more information on how to configure Synapse, including a complete accounting of
+# each option, go to docs/usage/configuration/config_documentation.md or
+# https://matrix-org.github.io/synapse/latest/usage/configuration/config_documentation.html
+#
+# This is set in /etc/matrix-synapse/conf.d/server_name.yaml for Debian installations.
+server_name: "$server_IP"
+pid_file: "/var/run/matrix-synapse.pid"
+listeners:
+  - port: 8008
+    tls: false
+    type: http
+    x_forwarded: true
+    bind_addresses: ['::1', '127.0.0.1']
+    resources:
+      - names: [client, federation]
+        compress: false
+database:
+  name: sqlite3
+  args:
+    database: /var/lib/matrix-synapse/homeserver.db
+log_config: "/etc/matrix-synapse/log.yaml"
+media_store_path: /var/lib/matrix-synapse/media
+signing_key_path: "/etc/matrix-synapse/homeserver.signing.key"
+trusted_key_servers:
+  - server_name: "matrix.org"
+trusted_proxies:
+  - 127.0.0.1
+enable_registration: true
+registration_shared_secret: "$strong_passwd"
 
-echo 'enable_registration: false' | tee -a /etc/matrix-synapse/homeserver.yaml
+email:
+  smtp_host: "smtp.163.com"
+  smtp_port: 465
+  smtp_user: "liuxiantao328@163.com"
+  smtp_pass: "VNBGPCYDIJNEUBAS"
+  require_transport_security: true
+  notif_from: "Your App Name <no-reply@matrix.com>"
+  
+enable_password_reset: true
 
-echo "registration_shared_secret: \"$strong_passwd\"" | tee -a /etc/matrix-synapse/homeserver.yaml
+public_baseurl: "https://$server_IP/"
+EOL
+
+# 输出成功信息
+echo "已成功更新 homeserver.yaml 文件。"
 
 systemctl restart matrix-synapse
